5286f001c18563bd7b5d79b91b081c871894d12f
Igor Amarilha <igoramarilha23@gmail.com>
2023-03-15 21:39:36 -0300

    Update API_teste.py

--- API_teste.py
+++ API_teste.py
@@ -1,11 +1,11 @@
-form  flask import Flask, jsonify, request
+from  flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 itens = [
-    {'id': 1 ,'nome': 'Livro', 'valor': 35 }
-    {'id': 2 ,'nome': 'lápis', 'valor': 1 }
-    {'id': 3 ,'nome': 'caderno', 'valor': 15 }
+    {'id': 1 ,'nome': 'Livro', 'valor': '35' }
+    {'id': 2 ,'nome': 'lápis', 'valor': '1' }
+    {'id': 3 ,'nome': 'caderno', 'valor': '15' }
 ]
 
 @app.route('/itens')
@@ -13,4 +13,4 @@ def  consultar_itens():
     return jsonify(itens)
 
 
-app.run(port=5000,host='localhost')
+app.run(port=5000,host='localhost',debug=True)
